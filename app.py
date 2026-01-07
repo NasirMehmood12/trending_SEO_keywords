e2421742301cb57c493169e15494738597b4af89
Nasir_Mehmood <nasirjani757571@gmail.com>
2026-01-07 13:55:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -713,7 +713,7 @@ GOOGLE_CREDENTIALS_JSON = os.environ.get("GOOGLE_CREDENTIALS_PATH")
 CRED_FILE = None
 if GOOGLE_CREDENTIALS_JSON:
     _FILE = "/tmp/google_credentials.json"
-    with open(_FILE, "w") as f:
+    with open(CRED_FILE, "w") as f:
         f.write(GOOGLE_CREDENTIALS_JSON)
 
 
@@ -1305,6 +1305,7 @@ if __name__ == '__main__':
     print("Starting Keyword Selection App...")
     print("Open http://localhost:5000 in your browser")
     socketio.run(app, host='0.0.0.0', port=10000, debug=False)
+
 
 
 
